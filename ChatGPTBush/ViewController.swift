ab46c6128b08e53f271d4c2a65f2139ca8883f8e
James Alan Bush <jamesalanbush1972@gmail.com>
2023-11-19 11:20:42 -0500

    Asks: Why is the sky blue? Answers.

--- ChatGPTBush/ViewController.swift
+++ ChatGPTBush/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var chatTextView: UITextView!
     @IBOutlet weak var chatButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Call the async function
@@ -21,41 +22,45 @@ class ViewController: UIViewController {
     
     @IBAction func chatAction(_ sender: Any) {
         print("Task")
-//        {
-            // Configure the OpenAI API with your API key
-            var openAI = OpenAI(apiToken: "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")
-
-            // Create a prompt
-            let query = CompletionsQuery(model: .gpt4_1106_preview, prompt: "Why is the sky blue?", temperature: 0, maxTokens: 100, topP: 1, frequencyPenalty: 0, presencePenalty: 0, stop: ["\\n"])
+        //        {
+        // Configure the OpenAI API with your API key
+        //        var openAI = OpenAI(apiToken: "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")
+        let configuration = OpenAI.Configuration(token: "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", organizationIdentifier: "org-jGOqXYFRJHKlnkff8K836fK2", timeoutInterval: 60.0)
+        let openAI = OpenAI(configuration: configuration)
+        
+        // Create a prompt
+        let query = CompletionsQuery(model: .textDavinci_003, prompt: "Why is the sky blue?", temperature: 0, maxTokens: 100, topP: 1, frequencyPenalty: 0, presencePenalty: 0, stop: ["\\n"])
+        
+        do {
+            // Define the completion parameters
+            //                let result = try openAI.completions(query: ÷r)
             
-            do {
-                // Define the completion parameters
-//                let result = try openAI.completions(query: ÷r)
-                
-                // Send the request to OpenAI's API
-                openAI.completions(query: query, completion: { completion in
-                    switch completion {
-                    case .success(let response):
-                        // Print the response from GPT
-                        if let text = response.choices.first?.text {
-                            print("Response: \(text)")
-                        } else {
-                            print("No text received in response.")
-                        }
-                    case .failure(let error):
-                        // Handle any errors
+            // Send the request to OpenAI's API
+            //            let chat_query = cha ChatQuery(model: .gpt4, messages: [Chat](Chat(role: .user), Chat(role: .user)))// ["role": "user", "content": "Why is the sky blue?"])
+            
+            openAI.completions(query: query) { completion in
+                switch completion {
+                case .success(let response):
+                    // Print the response from GPT
+                    if let text = response.choices.first {
                         DispatchQueue.main.async {
-                            self.chatTextView.text = "Error: \(error.localizedDescription)"
+                            self.chatTextView.text = "Response: \(text)"
+                        }
+                        
+                    } else {
+                        DispatchQueue.main.async {
+                            self.chatTextView.text = "No text received in response."
                         }
                     }
-                })
-            } catch {
-                
+                case .failure(let error):
+                    // Handle any errors
+                    DispatchQueue.main.async {
+                        self.chatTextView.text = "Error: \(error.localizedDescription)"
+                    }
+                }
             }
-//        }()
-        
-        //        Task {
-        //            await chat
-        //        }
+        } catch {
+            
+        }
     }
 }

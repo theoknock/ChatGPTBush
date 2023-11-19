80ac5e3f2f341b9a16c9d76cc26a9908ab403306
James Alan Bush <jamesalanbush1972@gmail.com>
2023-11-19 08:02:04 -0500

    Initial Commit

---
+++ ChatGPTBush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPTBush
+//
+//  Created by Xcode Developer on 11/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

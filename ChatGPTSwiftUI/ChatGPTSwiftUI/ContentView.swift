970350b05e26bacdc9d6717c06599fdcdde59d24
James Alan Bush <jamesalanbush1972@gmail.com>
2023-12-02 01:57:49 -0500

    TextEditor increases height on a per-line basis; gets focus on startup

--- ChatGPTSwiftUI/ChatGPTSwiftUI/ContentView.swift
+++ ChatGPTSwiftUI/ChatGPTSwiftUI/ContentView.swift
@@ -26,149 +26,127 @@ struct Message: Identifiable, Equatable, Hashable {
 
 struct Completion: Identifiable, Equatable, Hashable {
     let id: String
+    let date: String
     var messages: [Message] = [Message]()
 }
+
+struct ViewHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = value + nextValue()
+    }
+}
+
+//struct ContentView: View {
+//    @State private var text: String = ""
+//    @FocusState private var isTextEditorFocused: Bool
+//    
+//    var body: some View {
+//        TextEditor(text: $text)
+//            .focused($isTextEditorFocused)
+//            .onAppear {
+//                // This will set the focus on the TextEditor when the view appears
+//                isTextEditorFocused = true
+//            }
+//    }
+//}
 
 
 struct ContentView: View {
     @State private var senderMessage: String = ""
     @State private var receiverMessage: String = ""
     @State private var isTaskRunning = false
+    @FocusState private var isTextEditorFocused: Bool
+    @State var height: CGFloat = 20
     @State private var completions: [Completion] = [Completion]()
     
     
     var body: some View {
         GeometryReader(content: { geometry in
             VStack {
-                VStack {
-                    List {
-                        ForEach(completions) { completion in
-                            Section {
-                                ForEach(completion.messages) { message in
-                                    VStack {
-                                        Section {
-                                            Text(message.text)
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                .cornerRadius(10, antialiased: true)
-                                                .foregroundColor(Color.primary)
-                                        } header: {
-                                            Text((message.type == MessageType.prompt) ? "Prompt" : "Response")
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                                .cornerRadius(10, antialiased: true)
-                                                .listRowSeparator(.hidden)
-                                                .foregroundColor(Color.secondary)
-                                        }
-                                        .listSectionSeparator(.hidden)
-                                        .preferredColorScheme(.dark)
-                                        .padding(.horizontal)
-                                        
-                                    }
-                                    .frame(width: geometry.size.width , alignment: .leading)
-                                    .padding(.horizontal)
+                List {
+                    ForEach(completions) { completion in
+                        Section {
+                            ForEach(completion.messages) { message in
+                                Section {
+                                    Text(message.text)
+                                        .foregroundColor(Color.primary)
+                                } header: {
+                                    Text((message.type == MessageType.prompt) ? "Prompt" : "Response")
+                                        .listRowSeparator(.hidden)
+                                        .foregroundColor(Color.secondary)
                                 }
-                                .frame(width: geometry.size.width, alignment: .leading)
-                                .padding(.horizontal)
                             }
+                        } header: {
+                            Text(completion.date)
                         }
                     }
-                    HStack {
-                        TextEditor(text: $senderMessage)
-                            .multilineTextAlignment(.leading)
-                            .border(Color.secondary, width: 3.0)
-                            .onKeyPress(.return, action: {
-                                sendMessage(senderMessage)
-                                return .handled
-                            })
-                        Button(action: {
-                            sendMessage(senderMessage)
-                            self.isTaskRunning.toggle()
-                        }) {
-                            Image(systemName: self.isTaskRunning ? "play.fill" : "play")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .symbolRenderingMode(.monochrome)
-                                .fontWeight(.thin)
-                                .foregroundStyle(Color.secondary)
-                        }
-                        .border(Color.secondary, width: 3.0)
-                    }
-                    .frame(height: geometry.size.height * 0.0625, alignment: .leading)
                 }
+                
+                HStack {
+                                        ZStack(alignment: .leading) {
+                                            Text(senderMessage)
+                                                .lineLimit(3)
+                                                .font(.callout)
+                                                .padding(8)
+                                                .background(GeometryReader {
+                                                    Color.clear.preference(key: ViewHeightKey.self,
+                                                                           value: $0.frame(in: .local).size.height)
+                                                })
+                                                .hidden()
+                                            TextEditor(text: $senderMessage)
+                                                .font(.callout)
+                                                .frame(height: max(38,height))
+                                                .padding(.horizontal, 3)
+                                                .border(Color.primary, width: 1.0)
+                                                .lineLimit(3)
+                                        }
+                                        .background(Color.secondary.opacity(0.3))
+                                        .cornerRadius(8)
+                                        .onPreferenceChange(ViewHeightKey.self) { height = $0 }
+                                                            .padding()
+                    
+                    //                        HStack {
+//                    TextEditor(text: $senderMessage)
+//                        .autocorrectionDisabled(true)
+//                        .multilineTextAlignment(.leading)
+//                        .border(Color.secondary, width: 0.08125)
+//                        .onKeyPress(.return, action: {
+//                            sendMessage(senderMessage)
+//                            return .handled
+//                        })
+//                        .focused($isTextEditorFocused)
+//                        .onAppear {
+//                            // This will set the focus on the TextEditor when the view appears
+//                            isTextEditorFocused = true
+//                        }
+                    Button(action: {
+                        sendMessage(senderMessage)
+                    }) {
+                        Image(systemName: self.isTaskRunning ? "play.fill" : "play")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .symbolRenderingMode(.monochrome)
+                            .fontWeight(.thin)
+                            .foregroundStyle(Color.secondary)
+                    }
+                    .frame(height: geometry.size.height * 0.05, alignment: .center)
+                }
+                .preferredColorScheme(.dark)
             }
-            .preferredColorScheme(.dark)
         })
     }
     
-    //        GeometryReader(content: { geometry in
-    //            VStack {
-    //                VStack {
-    //                    Group {
-    //
-    //                        List(messages) { message in
-    //                            Section {
-    //                                Text(message.text)
-    //                                    .frame(maxWidth: .infinity, alignment: .leading)
-    //                                    .cornerRadius(10, antialiased: true)
-    //                                    .foregroundColor(Color.primary)
-    //                            } header: {
-    //                                Text((message.type == .prompt) ? "Prompt" : "Response")
-    //                                    .frame(maxWidth: .infinity, alignment: .leading)
-    //                                    .cornerRadius(10, antialiased: true)
-    //                                    .listRowSeparator(.hidden)
-    //                                    .foregroundColor(Color.secondary)
-    //                            }
-    //                            .listSectionSeparator(.hidden)
-    //                        }
-    //                        .listStyle(.insetGrouped)
-    //                        .preferredColorScheme(.dark)
-    //                    }
-    //                }
-    //                .border(Color.secondary, width: 3.0)
-    //
-    //                HStack {
-    //                    TextEditor(text: $senderMessage)
-    //                        .multilineTextAlignment(.leading)
-    //                        .border(Color.secondary, width: 3.0)
-    //                        .onKeyPress(.return, action: {
-    //                            sendMessage(senderMessage)
-    //                            return .handled
-    //                        })
-    //                    Button(action: {
-    //                        sendMessage(senderMessage)
-    //                    }) {
-    //                        Image(systemName: "play")
-    //                            .resizable()
-    //                            .aspectRatio(contentMode: .fit)
-    //                            .symbolRenderingMode(.monochrome)
-    //                            .fontWeight(.thin)
-    //                            .foregroundStyle(Color.secondary)
-    //                    }
-    //                    .border(Color.secondary, width: 3.0)
-    //                }
-    //                .frame(height: geometry.size.height * 0.0625, alignment: .leading)
-    //            }
-    //        })
-    //    }
-    
-    //    private func addMessage(completion: inout Completion, text: String, type: MessageType) {
-    //        guard !text.isEmpty else { return }
-    //        let newMessage = Message(idtext: text, type: type)
-    ////        var newChat = Completion()
-    //        let index = (completion.messages.count != 0) ? completion.messages.count - 1 : 0
-    //        completion.messages[index] = newMessage //.append(newMessage)
-    ////        completions.append(newChat)
-    //    }
-    
     private func sendMessage(_ message: String) {
         let tidyMessage = message.trimmingCharacters(in: .whitespacesAndNewlines)
-        
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        senderMessage = ""
         let url = URL(string: "https://api.openai.com/v1/chat/completions")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer ", forHTTPHeaderField: "Authorization")
-        
         request.addValue("org-jGOqXYFRJHKlnkff8K836fK2", forHTTPHeaderField: "OpenAI-Organization")
         
         let payload: [String: Any] = [
@@ -184,17 +162,15 @@ struct ContentView: View {
         ]
         
         let jsonData = try! JSONSerialization.data(withJSONObject: payload, options: [])
-        
         request.httpBody = jsonData
         
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             DispatchQueue.main.async {
-                var completion: Completion = Completion(id: sha256(), messages: [Message]())
+                var completion: Completion = Completion(id: sha256(), date: Date().description, messages: [Message]())
                 completion.messages.append(Message(id: sha256(), text: tidyMessage, type: MessageType.prompt))
-                senderMessage = ""
                 if (data != nil) {
-//                    completion.messages.append(Message(id: sha256(), text: String(data: data!, encoding: .utf8)!, type: MessageType.prompt))
+                    //                    completion.messages.append(Message(id: sha256(), text: String(data: data!, encoding: .utf8)!, type: MessageType.prompt))
                     do {
                         if let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any],
                            let choices = jsonResponse["choices"] as? [[String: Any]],
@@ -208,36 +184,37 @@ struct ContentView: View {
                     }
                 }
                 if (response != nil) {
-//                    completion.messages.append(Message(id: sha256(), text: "respond", type: MessageType.response))
+                    //                    completion.messages.append(Message(id: sha256(), text: "\(response)", type: MessageType.response))
                 }
                 if (error != nil) {
                     completion.messages.append(Message(id: sha256(), text: "Error: \(error!.localizedDescription)", type: MessageType.response))
                 }
-                print(completion)
+                //                print(completion)
                 completions.append(completion)
             }
         }
-            task.resume()
+        task.resume()
         
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             switch task.state {
             case .running:
                 print("Task is still running...")
+                self.isTaskRunning = true
             case .completed:
                 print("Task completed")
-                self.isTaskRunning.toggle()
+                self.isTaskRunning = false
                 timer.invalidate()
             case .canceling:
                 print("Task is canceling")
-                self.isTaskRunning.toggle()
+                self.isTaskRunning = false
                 timer.invalidate()
             case .suspended:
                 print("Task is suspended")
-                self.isTaskRunning.toggle()
+                self.isTaskRunning = false
                 timer.invalidate()
             @unknown default:
                 print("Unknown state")
-                self.isTaskRunning.toggle()
+                self.isTaskRunning = false
                 timer.invalidate()
             }
         }
@@ -249,3 +226,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//struct P239_DynamicTextEditor_Previews: PreviewProvider {
+//    static var previews: some View {
+//        P239_DynamicTextEditor()
+//    }
+//}
